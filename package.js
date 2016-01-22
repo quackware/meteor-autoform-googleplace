b6c115894b2541c842401bb36e75c7aa16f5a2d5
Curtis Larson <quackware@gmail.com>
2016-01-22 08:29:55 -0500

    Edit package.js for publishing purposes

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'lukemadera:autoform-googleplace',
+  name: 'quackware:lukemadera-autoform-googleplace',
   version: '1.0.7',
   // Brief, one-line summary of the package.
   summary: 'Google Places Autocomplete input (without map). Returns an object with formatted address components',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/lukemadera/meteor-autoform-googleplace',
+  git: 'https://github.com/quackware/meteor-autoform-googleplace',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
